5d4b90539bc8ff590301be5d9ba103581f35dc9b
rajeshsharma88 <91082296+rajeshsharma88@users.noreply.github.com>
2021-10-26 15:22:36 +0530

    Stated

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+
+const Header = (props) => {
+  return;
+  <AppBar>
+    <ToolBar>Pixelris</ToolBar>
+  </AppBar>;
+};
+
+export default Header;
